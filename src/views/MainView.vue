9384c20813b727867d116c6612c9f20d0543cf74
Peterson Paulo <peterpaulodev@gmail.com>
2022-08-09 19:29:10 -0300

    add gif

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,37 +1,40 @@
 <template>
-  <section id="characters" class="col-md-12 mt-5 background-pacific-blue">
-    <h1 class="text-center fw-bold text-white mt-4">Personagens</h1>
+    <div class="row wave-bg" id="about-section">
 
-    <div class="input-group mx-auto my-4 w-50 col-md-12">
-      <input
-        type="text"
-        class="form-control"
-        id="character-name"
-        placeholder="Digite para procurar..."
-        aria-label="Digite para procurar..."
-        aria-describedby="basic-addon2"
-        v-model="pesquisaChar"
-      />
-      <span class="input-group-text" id="basic-addon2">
-        <i class="fas fa-search" aria-hidden="true"></i>
-      </span>
+        <section id="header" class="col-md-12 text-center mb-4 mt-5">
+            <h1 class="fw-bold">Rick and Morty Vue API</h1>
+            <img src="../assets/img/dancing-rick.gif" class="mb-3 mt-5" />
+        </section>
+
+        <section id="characters" class="col-md-12 mt-5 background-pacific-blue">
+            <h1 class="text-center fw-bold text-white mt-4 pt-3">Personagens</h1>
+
+            <div class="input-group mx-auto my-4 w-50 col-md-12">
+                <input type="text" class="form-control" id="character-name" placeholder="Digite para procurar..."
+                    aria-label="Digite para procurar..." aria-describedby="basic-addon2" v-model="pesquisaChar" />
+                <span class="input-group-text" id="basic-addon2">
+                    <i class="fas fa-search" aria-hidden="true"></i>
+                </span>
+            </div>
+            <div class="cards row mx-auto my-4">
+                <PersonagemView :pesquisaChar="pesquisaChar" />
+            </div>
+        </section>
     </div>
-    <div class="cards row mx-auto my-4">
-      <PersonagemView :pesquisaChar="pesquisaChar" />
-    </div>
-  </section>
 </template>
 
 <script>
-import PersonagemView from "./PersonagemView.vue";
-export default {
-  components: { PersonagemView },
-  data() {
-    return {
-      pesquisaChar: "",
+    import PersonagemView from "./PersonagemView.vue";
+    export default {
+        components: {
+            PersonagemView
+        },
+        data() {
+            return {
+                pesquisaChar: "",
+            };
+        },
     };
-  },
-};
 </script>
 
 <style>
